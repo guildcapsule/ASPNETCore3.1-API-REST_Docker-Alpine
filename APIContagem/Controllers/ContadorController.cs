933350e0c7f9bc5dae4ca5089bd88a0b9b6eecd7
karine santos <karine.santos@luizalabs.com>
2022-10-04 20:57:29 -0300

    feat: developer test branch

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Developer test branch",
+                    Local = "New developer test branch",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
